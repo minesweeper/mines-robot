ab1a16084df5cdcf4bb7cf7157ff64d01b0aa0ad
Mark Ryall <mark@ryall.name>
2016-07-21 20:27:12 +1000

    use rudimentary bestGuess and obviousMines functions in each turn

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,23 @@
-const markMines = (game) => {};
-const revealSafeCells = (game) => {};
-const guess = (game) => {};
+import bestGuess from './bestGuess';
+import obviousMines from './obviousMines';
+import {each} from 'lodash';
+
+const markMines = (game) => {
+  const mines = obviousMines(game);
+  each(mines, (cell) => { game.mark(cell); });
+  return mines;
+};
+
+const revealSafeCells = (game) => {
+  const safeCells = [];
+  each(safeCells, (cell) => { game.chord(cell); });
+  return safeCells;
+};
+
+const guess = (game) => {
+  const cell = bestGuess(game);
+  game.chord(cell);
+  return cell;
+};
 
 export default { markMines, revealSafeCells, guess };
