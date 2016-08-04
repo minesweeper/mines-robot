2a4602fa1407d50934fd96f0b3add30290d77eed
Alister Scott <alister.scott@automattic.com>
2016-08-04 16:28:50 +1000

    Clean up bestGuess as you always get cells now

--- src/bestGuess/index.js
+++ src/bestGuess/index.js
@@ -1,17 +1,6 @@
-import eachCell from '../eachCell';
-import {cellStates} from 'mines';
 import mineProbabilities from '../mineProbabilities';
 
 export default (game) => {
   const orderedCells = mineProbabilities(game);
-  if (orderedCells.length > 0) {
-    return orderedCells[0][0];
-  }
-  let bestGuess = null;
-  eachCell(game, (cell, cellState) => {
-    if (!bestGuess && cellState === cellStates.UNKNOWN) {
-      bestGuess = cell;
-    }
-  });
-  return bestGuess;
+  return orderedCells[0][0];
 };
